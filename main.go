093ac820a12482de5dea4a4b5fc81ed49162b1fc
marriosdev <barrigacraft@hotmail.com>
2025-05-05 23:59:30 -0300

    touchpad test

--- main.go
+++ main.go
@@ -2,22 +2,76 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
+
+	"github.com/go-vgo/robotgo"
+	"github.com/gorilla/websocket"
 )
 
+var ServerIp string
+var lastX, lastY int
+
+type HomeData struct {
+	ServerIp string
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func main() {
-	ip := getLocalIPv4()
+	ServerIp = getLocalIPv4()
 	port := "8080"
-	address := fmt.Sprintf("%s:%s", ip, port)
+	address := fmt.Sprintf("%s:%s", ServerIp, port)
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Olá! Você acessou %s\n", r.URL.Path)
-	})
+	http.HandleFunc("/", serveTouchpad)
+	http.HandleFunc("/ws", handleWebSocket)
 
-	fmt.Printf("Servidor rodando em http://%s\n", address)
+	fmt.Printf("Acesse o touchpad em http://%s\n", address)
 	log.Fatal(http.ListenAndServe(address, nil))
+}
+
+func serveTouchpad(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, HomeData{ServerIp: ServerIp})
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Erro ao subir WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	screenWidth, screenHeight := robotgo.GetScreenSize()
+
+	for {
+		var pos struct {
+			X float64 `json:"x"`
+			Y float64 `json:"y"`
+		}
+		err := conn.ReadJSON(&pos)
+		if err != nil {
+			log.Println("Erro ao ler dados WebSocket:", err)
+			break
+		}
+
+		normalizedX := int(pos.X * float64(screenWidth) / 1000)
+		normalizedY := int(pos.Y * float64(screenHeight) / 1000)
+
+		moveX := normalizedX - lastX
+		moveY := normalizedY - lastY
+
+		robotgo.MoveMouse(lastX+moveX, lastY+moveY)
+
+		lastX, lastY = lastX+moveX, lastY+moveY
+	}
 }
 
 func getLocalIPv4() string {
@@ -25,9 +79,7 @@ func getLocalIPv4() string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(conn)
 	defer conn.Close()
-
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP.String()
 }
